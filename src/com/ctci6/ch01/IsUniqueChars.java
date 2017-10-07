aaaf9d44a47f310b5382bd4c99c1f863ffdb4269
sunilsoni <sunilsoni@live.in>
2017-10-07 08:35:54 +0530

    first commit

---
+++ src/com/ctci6/ch01/IsUniqueChars.java
@@ -0,0 +1,17 @@
+package com.ctci6.ch01;
+
+/**
+ * Implement an algorithm to determine if a string has all unique characters.
+ * What if you cannot use additional data structures?
+ * 
+ * @author Sunil
+ *
+ */
+public class IsUniqueChars {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
