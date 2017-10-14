8219a1a36c08160544f50fdda0b15aadafeca162
Sunil Soni <sunil.soni@live.in>
2017-10-14 09:45:23 +0530

    Permutation

--- src/com/ctci6/ch01/Permutation.java
+++ src/com/ctci6/ch01/Permutation.java
@@ -1,5 +1,7 @@
 package com.ctci6.ch01;
 
+import java.util.Arrays;
+
 /**
  * Given two strings, write a method to decide if one is a permutation of the other.
  * 
@@ -10,7 +12,7 @@ public class Permutation {
 	public static void main(String[] args) {
 		String[][] pairs = {{"apple", "papel"}, {"carrot", "tarroc"}, {"hello", "llloh"}};
 		for (String[] pair : pairs) {
-			String word1 = pair[0]; 
+			String word1 = pair[0];   
 			String word2 = pair[1];
 			boolean anagram1 = permutation1(word1, word2);
 			System.out.println(word1 + ", " + word2 + ": " + anagram1);
@@ -36,9 +38,11 @@ public class Permutation {
 		
 		int[] letters = new int[128]; // Assumption: ASCII
 		for (int i = 0; i < s.length(); i++) {
+			System.out.println(s.charAt(i)+" ascii -->"+ (int)(s.charAt(i)));
 			letters[s.charAt(i)]++;  
 		}
-		  
+		System.out.println("letters-->"+Arrays.toString(letters));
+		
 		for (int i = 0; i < t.length(); i++) {
 			letters[t.charAt(i)]--;
 		    if (letters[t.charAt(i)] < 0) {
@@ -49,3 +53,11 @@ public class Permutation {
 		return true; // letters array has no negative values, and therefore no positive values either
 	}
 }
+/*
+ * 
+ * ASCII defines 128 characters, which map to the numbers 0–127. Unicode defines (less than) 221 characters, which, similarly, map to numbers 0–221 (though not all numbers are currently assigned, and some are reserved).
+
+Unicode is a superset of ASCII, and the numbers 0–128 have the same meaning in ASCII as they have in Unicode. For example, the number 65 means "Latin capital 'A'".
+
+Because Unicode characters don't generally fit into one 8-bit byte, there are numerous ways of storing Unicode characters in byte sequences, such as UTF-32 and UTF-8.
+*/
