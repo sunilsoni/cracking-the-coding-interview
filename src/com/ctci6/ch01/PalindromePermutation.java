0f10cb57c9ff3bdc0f0dad1e859c283c4e96c4b6
Sunil Soni <sunilsoni@live.in>
2017-10-15 10:32:24 +0530

    PalindromePermutation

--- src/com/ctci6/ch01/PalindromePermutation.java
+++ src/com/ctci6/ch01/PalindromePermutation.java
@@ -3,56 +3,69 @@ package com.ctci6.ch01;
 import java.util.Arrays;
 
 /**
- * Given a string, write a function to check if it is a permutation of a palindrome. 
- * A palindrome is a word or phrase that is the same forwards and backwards. 
- * A permutation is a rearrangement of letters. The palindrome does not need to be limited to just dictionary words. 
- * Details
- * Example
- * Input:     Tact coa
- * Output:    True (permutation:  “taco  cat”, “atco  cta”, etc . )
+ * Given a string, write a function to check if it is a permutation of a
+ * palindrome. A palindrome is a word or phrase that is the same forwards and
+ * backwards. A permutation is a rearrangement of letters. The palindrome does
+ * not need to be limited to just dictionary words. Details Example Input: Tact
+ * coa Output: True (permutation: “taco cat”, “atco cta”, etc . )
+ * 
  * @URL https://www.hackerearth.com/problem/algorithm/palindrome-check-2-1/
  * @author Sunil
  */
 public class PalindromePermutation {
 
 	public static void main(String[] args) {
-
-
+		String[] strings = { "Rats live on no evil star", "A man, a plan, a canal, panama", "Lleve", "Tacotac",
+				"asda" };
+		for (String s : strings) {
+			boolean a = isPermutationOfPalindrome1(s);
+			boolean b = isPermutationOfPalindrome2(s);
+			boolean c = isPermutationOfPalindrome3(s);
+			System.out.println(s);
+			if (a == b && b == c) {
+				System.out.println("Agree: " + a);
+			} else {
+				System.out.println("Disagree: " + a + ", " + b + ", " + c);
+			}
+			System.out.println();
+		}
 
 	}
 
-	/* Map each character to a number. a -) 0J b - ) IJ C - ) 2J etc.
-	 * This is case insensitive. Non-letter characters map to -1. */
+	/*
+	 * Map each character to a number. a -) 0J b - ) IJ C - ) 2J etc. This is case
+	 * insensitive. Non-letter characters map to -1.
+	 */
 	public static int getCharNumber(Character c) {
 		int a = Character.getNumericValue('a');
 		int z = Character.getNumericValue('z');
-		
+
 		int val = Character.getNumericValue(c);
 		if (a <= val && val <= z) {
 			return val - a;
 		}
 		return -1;
 	}
-	
+
 	/* Count how many times each character appears. */
 	public static int[] buildCharFrequencyTable(String phrase) {
-		System.out.println("z getNumericValue-->"+Character.getNumericValue('z'));
-		System.out.println("a getNumericValue-->"+Character.getNumericValue('a'));
+		System.out.println("z getNumericValue-->" + Character.getNumericValue('z'));
+		System.out.println("a getNumericValue-->" + Character.getNumericValue('a'));
 		int[] table = new int[Character.getNumericValue('z') - Character.getNumericValue('a') + 1];
 		for (char c : phrase.toCharArray()) {
 			int x = getCharNumber(c);
-			System.out.println(c+"-->"+x);
+			System.out.println(c + "-->" + x);
 			if (x != -1) {
 				table[x]++;
 			}
 		}
 		return table;
 	}
-	
+
 	/**
 	 * This algorithm takes O(N) time, where N is the length of the string
 	 */
-	
+
 	/* Check that no more than one character has an odd count. */
 	public static boolean checkMaxOneOdd(int[] table) {
 		boolean foundOdd = false;
@@ -66,13 +79,14 @@ public class PalindromePermutation {
 		}
 		return true;
 	}
-	
+
 	public static boolean isPermutationOfPalindrome1(String phrase) {
 		int[] table = buildCharFrequencyTable(phrase);
-		System.out.println("table-->"+Arrays.toString(table));//[2, 0, 0, 0, 2, 0, 0, 0, 2, 0, 0, 2, 0, 2, 2, 0, 0, 2, 2, 2, 0, 2, 0, 0, 0, 0]
+		System.out.println("table-->" + Arrays.toString(table));// [2, 0, 0, 0, 2, 0, 0, 0, 2, 0, 0, 2, 0, 2, 2, 0, 0,
+																// 2, 2, 2, 0, 2, 0, 0, 0, 0]
 		return checkMaxOneOdd(table);
 	}
-	
+
 	/**
 	 * This algorithm also takes O(N) time, where N is the length of the string
 	 */
@@ -93,15 +107,16 @@ public class PalindromePermutation {
 		}
 		return countOdd <= 1;
 	}
-	
+
 	/**
 	 * This algorithm also takes O(N) time, where N is the length of the string
 	 */
-	
+
 	/* Toggle the ith bit in the integer. */
 	public static int toggle(int bitVector, int index) {
-		if (index < 0) return bitVector;
-		
+		if (index < 0)
+			return bitVector;
+
 		int mask = 1 << index;
 		if ((bitVector & mask) == 0) {
 			bitVector |= mask;
@@ -110,9 +125,11 @@ public class PalindromePermutation {
 		}
 		return bitVector;
 	}
-	
-	/* Create bit vector for string. For each letter with value i,
-	 * toggle the ith bit. */
+
+	/*
+	 * Create bit vector for string. For each letter with value i, toggle the ith
+	 * bit.
+	 */
 	public static int createBitVector(String phrase) {
 		int bitVector = 0;
 		for (char c : phrase.toCharArray()) {
@@ -121,16 +138,18 @@ public class PalindromePermutation {
 		}
 		return bitVector;
 	}
-	
-	/* Check that exactly one bit is set by subtracting one from the 
-	 * integer and ANDing it with the original integer. */
+
+	/*
+	 * Check that exactly one bit is set by subtracting one from the integer and
+	 * ANDing it with the original integer.
+	 */
 	public static boolean checkExactlyOneBitSet(int bitVector) {
 		return (bitVector & (bitVector - 1)) == 0;
 	}
-	
+
 	public static boolean isPermutationOfPalindrome3(String phrase) {
 		int bitVector = createBitVector(phrase);
 		return bitVector == 0 || checkExactlyOneBitSet(bitVector);
 	}
-	
+
 }
