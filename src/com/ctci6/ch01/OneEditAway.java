2848061d5dbac6ec3b7044bfb4163eca904e9cb4
Sunil Soni <sunilsoni@live.in>
2017-10-15 17:21:29 +0530

    OneEditAway

--- src/com/ctci6/ch01/OneEditAway.java
+++ src/com/ctci6/ch01/OneEditAway.java
@@ -15,9 +15,96 @@ package com.ctci6.ch01;
 public class OneEditAway {
 
 	public static void main(String[] args) {
+		String a = "pse";
+		String b = "pale";
+		boolean isOneEdit = oneEditAway1(a, b);
+		System.out.println(a + ", " + b + ": " + isOneEdit);
+		System.out.println("----------------------");
+		
+		a = "palee";
+		b = "pale";
+		boolean isOneEdit1 = oneEditAway2(a, b);
+		System.out.println(a + ", " + b + ": " + isOneEdit1);
 
-
-
+		String c = "pale";
+		String d = "pkle";
+		boolean isOneEdit2 = oneEditAway2(c, d);
+		System.out.println(c + ", " + d + ": " + isOneEdit2);
 	}
+	
+	
+	
+	public static boolean oneEditReplace(String s1, String s2) {
+		boolean foundDifference = false;
+		for (int i = 0; i < s1.length(); i++) {
+			if (s1.charAt(i) != s2.charAt(i)) {
+				if (foundDifference) {
+					return false;
+				}
+				
+				foundDifference = true;
+			}
+		}
+		return true;
+	}
+	
+	/* Check if you can insert a character into s1 to make s2. */
+	public static boolean oneEditInsert(String s1, String s2) {
+		int index1 = 0;
+		int index2 = 0;
+		while (index2 < s2.length() && index1 < s1.length()) {
+			if (s1.charAt(index1) != s2.charAt(index2)) {
+				if (index1 != index2) {
+					return false;
+				}		
+				index2++;
+			} else {
+				index1++;
+				index2++;
+			}
+		}
+		return true;
+	}	
+	
+	public static boolean oneEditAway1(String first, String second) {
+		if (first.length() == second.length()) {
+			return oneEditReplace(first, second);
+		} else if (first.length() + 1 == second.length()) {
+			return oneEditInsert(first, second);
+		} else if (first.length() - 1 == second.length()) {
+			return oneEditInsert(second, first);
+		} 
+		return false;
+	}
+	
+	public static boolean oneEditAway2(String first, String second) {
+		/* Length checks. */
+		if (Math.abs(first.length() - second.length()) > 1) {
+			return false;
+		}
+		
+		/* Get shorter and longer string.*/
+		String s1 = first.length() < second.length() ? first : second;
+		String s2 = first.length() < second.length() ? second : first;
 
+		int index1 = 0;
+		int index2 = 0;
+		boolean foundDifference = false;
+		while (index2 < s2.length() && index1 < s1.length()) {
+			if (s1.charAt(index1) != s2.charAt(index2)) {
+				/* Ensure that this is the first difference found.*/
+				if (foundDifference) return false;
+				foundDifference = true;
+				if (s1.length() == s2.length()) { // On replace, move shorter pointer
+					index1++;
+				}
+			} else {
+				index1++; // If matching, move shorter pointer 
+			}
+			index2++; // Always move pointer for longer string 
+		}
+		return true;
+	}
+	
+	
 }
