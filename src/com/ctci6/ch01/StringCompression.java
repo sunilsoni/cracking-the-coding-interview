64eb8ffb780b35e2dca485c546e039492b108698
Sunil Soni <sunilsoni@live.in>
2017-10-19 07:09:26 +0530

    StringCompression

---
+++ src/com/ctci6/ch01/StringCompression.java
@@ -0,0 +1,21 @@
+package com.ctci6.ch01;
+
+/**
+ * Implement a method to perform basic string compression using the counts
+ * of repeated characters. 
+ * <blockquote><pre> For example, 
+ * The string aabcccccaaa would become a2blc5a3. </pre></blockquote>
+ * If the "compressed"string would not become smaller than the original string, your method should return
+ * the original string. You can assume the string has only uppercase and lowercase letters (a - z).
+ * 
+ * @author Sunil
+ */
+public class StringCompression {
+
+	public static void main(String[] args) {
+
+
+
+	}
+
+}
