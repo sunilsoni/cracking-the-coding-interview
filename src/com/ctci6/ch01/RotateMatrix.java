7def30e807089e1d170d4c62834751dd5bc73b06
Sunil Soni <sunilsoni@live.in>
2017-10-19 10:11:03 +0530

    RotateMatrix

--- src/com/ctci6/ch01/RotateMatrix.java
+++ src/com/ctci6/ch01/RotateMatrix.java
@@ -1,5 +1,7 @@
 package com.ctci6.ch01;
 
+import com.ctci6.utils.AssortedMethods;
+
 /**
  * Given an image represented by an NxN matrix, where each pixel in the image is 4
  * bytes, write a method to rotate the image by 90 degrees. Can you do this in place?
@@ -9,8 +11,37 @@ package com.ctci6.ch01;
 public class RotateMatrix {
 
 	public static void main(String[] args) {
-		 
-
+		int[][] matrix = AssortedMethods.randomMatrix(3, 3, 0, 9);
+		AssortedMethods.printMatrix(matrix);
+		rotate(matrix);
+		System.out.println();
+		AssortedMethods.printMatrix(matrix);
 	}
 
+	public static boolean rotate(int[][] matrix) {
+		if (matrix.length == 0 || matrix.length != matrix[0].length) return false; // Not a square
+		int n = matrix.length;
+		
+		for (int layer = 0; layer < n / 2; layer++) {
+			int first = layer;
+			int last = n - 1 - layer;
+			for(int i = first; i < last; i++) {
+				int offset = i - first;
+				int top = matrix[first][i]; // save top
+
+				// left -> top
+				matrix[first][i] = matrix[last-offset][first]; 			
+
+				// bottom -> left
+				matrix[last-offset][first] = matrix[last][last - offset]; 
+
+				// right -> bottom
+				matrix[last][last - offset] = matrix[i][last]; 
+
+				// top -> right
+				matrix[i][last] = top; // right <- saved top
+			}
+		}
+		return true;
+	}
 }
