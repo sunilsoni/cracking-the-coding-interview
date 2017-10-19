5ef295651c65e67a299d3184ecbcdbf7a862d684
Sunil Soni <sunilsoni@live.in>
2017-10-19 08:53:57 +0530

    StringCompression

--- src/com/ctci6/ch01/StringCompression.java
+++ src/com/ctci6/ch01/StringCompression.java
@@ -18,6 +18,18 @@ public class StringCompression {
 		
 		str = "aa";
 		System.out.println(compress1(str));
+		
+		str = "aabcccccaaa";
+		System.out.println(compress1(str));
+		System.out.println("=============");
+		str = "b";
+		System.out.println(compress2(str));
+		
+		str = "aa";
+		System.out.println(compress2(str));
+		
+		str = "aabcccccaaa";
+		System.out.println(compress2(str));
 	}
 	
 	public static String compress1(String str) {
@@ -29,7 +41,6 @@ public class StringCompression {
 		
 		StringBuilder compressed = new StringBuilder();
 		int countConsecutive = 0;
-		boolean isCompressed=false;
 		System.out.println("size--->"+size);
 		for (int i = 0; i < size; i++) {
 			countConsecutive++;
@@ -39,15 +50,44 @@ public class StringCompression {
 				compressed.append(str.charAt(i));
 				compressed.append(countConsecutive);
 				countConsecutive = 0;
-				isCompressed=true;
 			}
 		}
-		if(isCompressed) {
-			return compressed.toString();
-		}
-		
-		return  str;
+		return compressed.length() < str.length() ? compressed.toString() : str;
 	}
+	
+	public static String compress2(String str) {
+		int finalLength = countCompression(str);
+		if (finalLength >= str.length()) return str;
+		
+		StringBuffer compressed = new StringBuffer(finalLength); // initialize capacity
+		int countConsecutive = 0;
+		for (int i = 0; i < str.length(); i++) {
+			countConsecutive++;
+			
+			/* If next character is different than current, append this char to result.*/
+			if (i + 1 >= str.length() || str.charAt(i) != str.charAt(i + 1)) {
+				compressed.append(str.charAt(i));
+				compressed.append(countConsecutive);
+				countConsecutive = 0;
+			}
+		}
+		return compressed.toString();
+	}
+	
+	public static int countCompression(String str) {
+		int compressedLength = 0;
+		int countConsecutive = 0;
+		for (int i = 0; i < str.length(); i++) {
+			countConsecutive++;
+			
+			/* If next character is different than current, append this char to result.*/
+			if (i + 1 >= str.length() || str.charAt(i) != str.charAt(i + 1)) {
+				compressedLength += 1 + String.valueOf(countConsecutive).length();
+				countConsecutive = 0;
+			}
+		}
+		return compressedLength;
+	}		
 	
 
 }
