5357131954dc4ce9170fd6a48d1f974076c1464c
Sunil Soni <sunilsoni@live.in>
2017-10-21 11:58:33 +0530

    Package

--- src/com/ctci6/ch01/StringRotation.java
+++ src/com/ctci6/ch01/StringRotation.java
@@ -1,9 +1,10 @@
 package com.ctci6.ch01;
 
 /**
- * Assume you have a method i5Substring which checks ifone word is a substring of another.
+ * Assume you have a method i5Substring which checks if one word is a substring of another.
  * Given two strings, 51 and 52, write code to check if 52 is a rotation of 51 using only one call to isSubstring:
  * <blockquote><pre>e.g., "Uwaterbottleu" is a rotation of "uerbottlewatU".</blockquote></pre>
+ * RunTime: o(n)
  * 
  * @author Sunil
  */
--- src/com/ctci6/ch02/RemoveDups.java
+++ src/com/ctci6/ch02/RemoveDups.java
@@ -1,9 +1,18 @@
 package com.ctci6.ch02;
 
+/**
+ * Write code to remove duplicates from an unsorted linked list.
+ * <p>FOLLOW UP</p>
+ * How would you solve this problem if a temporary buffer is not allowed?
+ * 
+ * @author Sunil
+ */
 public class RemoveDups {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+
+		
 
 	}
 
