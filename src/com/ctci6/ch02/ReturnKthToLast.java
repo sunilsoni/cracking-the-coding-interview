4ded6db8f560d5ea1b73cf828de088d3755fa855
Sunil Soni <sunilsoni@live.in>
2017-10-22 09:14:17 +0530

    ReturnKthToLast

--- src/com/ctci6/ch02/ReturnKthToLast.java
+++ src/com/ctci6/ch02/ReturnKthToLast.java
@@ -1,5 +1,10 @@
 package com.ctci6.ch02;
 
+/**
+ * Implement an algorithm to find the kth to last element of a singly linked list.
+ * 
+ * @author Sunil
+ */
 public class ReturnKthToLast {
 
 	public static void main(String[] args) {
