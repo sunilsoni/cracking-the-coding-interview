8c82f784ac792ced3025534172af6facdd575760
Sunil Soni <sunilsoni@live.in>
2017-10-24 06:46:24 +0530

    ReturnKthToLast

--- src/com/ctci6/ch02/ReturnKthToLast.java
+++ src/com/ctci6/ch02/ReturnKthToLast.java
@@ -19,6 +19,21 @@ public class ReturnKthToLast {
 		for (int i = 0; i <= array.length + 1; i++) {
 			printKthToLast(head, i);
 		}
+		
+		//Approach C: Create a Wrapper Class.
+		for (int i = 0; i <= array.length + 1; i++) {
+			LinkedListNode node = kthToLast(head, i);
+			String nodeValue = node == null ? "null" : "" + node.data;
+			System.out.println(i + ": " + nodeValue);
+		}
+		
+		
+		//Solution #3: Iterative
+		for (int i = 0; i <= array.length + 1; i++) {
+			LinkedListNode node = nthToLast(head, i);
+			String nodeValue = node == null ? "null" : "" + node.data;
+			System.out.println(i + ": " + nodeValue);
+		}
 	}
 	
 	/*
@@ -63,5 +78,22 @@ public class ReturnKthToLast {
 
 	
 	//Solution #3: Iterative
-
+	public static LinkedListNode nthToLast(LinkedListNode head, int k) {
+		LinkedListNode p1 = head;
+		LinkedListNode p2 = head;
+		
+		/* Move p1 k nodes into the list.*/
+		for (int i = 0; i < k; i++) {
+			if (p1 == null) return null; // Out of bounds
+			p1 = p1.next;
+		}
+		
+		/* Move them at the same pace. When p1 hits the end, 
+		 * p2 will be at the right element. */
+		while (p1 != null) {
+			p1 = p1.next;
+			p2 = p2.next;
+	  	}
+	  	return p2;
+	}
 }
