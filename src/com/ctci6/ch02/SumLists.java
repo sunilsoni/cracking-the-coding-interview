91262fe9d9a394a4243db1b2f64cda76c1d655d4
Sunil Soni <sunilsoni@live.in>
2017-10-28 08:50:00 +0530

    SumLists

---
+++ src/com/ctci6/ch02/SumLists.java
@@ -0,0 +1,24 @@
+package com.ctci6.ch02;
+
+/**
+ * You have two numbers represented by a linked list, where each node contains a single digit. The digits are stored in reverse order,
+ * such that the 1's digit is at the head of the list. Write a function that adds the two numbers and returns the sum as a linked list.
+
+ * <p>EXAMPLE:</p>
+ * <blockquote><pre>
+ * Input: (7-> 1 -> 6) + (5 -> 9 -> 2) .That is,617 + 295.
+ * Output: 2 - > 1 - > 9.That is, 912.
+ * </blockquote></pre>
+ * 
+ * <p>FOLLOW UP:</p>
+ * <blockquote><pre>
+ * Suppose the digits are stored in forward order. Repeat the above problem.
+ * Input: (6 -> 1 -> 7) + (2 -> 9 -> 5).That is,617 + 295.
+ * Output: 9 - > 1 - > 2.That is, 912.
+ * </blockquote></pre>
+ * 
+ * @author Sunil
+ */
+public class SumLists {
+
+}
