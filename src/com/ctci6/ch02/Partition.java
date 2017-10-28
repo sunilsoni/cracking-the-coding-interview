a127ea6327406900fade0f93936386376d4d0312
Sunil Soni <sunilsoni@live.in>
2017-10-28 08:02:13 +0530

    Partition

--- src/com/ctci6/ch02/Partition.java
+++ src/com/ctci6/ch02/Partition.java
@@ -5,12 +5,13 @@ import com.ctci6.utils.LinkedListNode;
 /**
  * Write code to partition a linked list around a value x, such that all nodes less than x come before all nodes greater than or equal to x.
  * If x is contained within the list, the values of x only need to be after the elements less than x (see below).
- * The partition element x can appear anywhere in the right partition"; it does not need to appear between the left and right partitions.
+ * The partition element x can appear anywhere in the "right partition"; it does not need to appear between the left and right partitions.
 
- * EXAMPLE
+ * <p>EXAMPLE:</p>
+ * <blockquote><pre>
  * Input: 3 -> 5 -> 8 -> 5 -> 113 -> 2 -> 1 [partition = 5]
  * Output: 3 -> 1 -> 2 -> 113 -> 5 -> 5 -> 8
-
+ * </blockquote></pre>
  * @author Sunil
  */
 public class Partition {
