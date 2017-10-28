aceae6e513aa13d806c632d6ceb4d4d47439ec33
Sunil Soni <sunilsoni@live.in>
2017-10-28 08:25:55 +0530

    Partition

--- src/com/ctci6/ch02/Partition.java
+++ src/com/ctci6/ch02/Partition.java
@@ -18,24 +18,32 @@ import com.ctci6.utils.LinkedListNode;
 public class Partition {
 
 	public static void main(String[] args) {
+			System.out.println("printForward--->"+createLinkedList().printForward());
+			
+			/* Partition */
+			LinkedListNode hA = partition1(createLinkedList(), 5);
+			LinkedListNode hB = partition2(createLinkedList(), 5);
+			LinkedListNode hC = partition3(createLinkedList(), 5);
+			
+			/* Print Result */
+			System.out.println("hA--->"+hA.printForward());
+			System.out.println("hB--->"+hB.printForward());
+			System.out.println("hC--->"+hC.printForward());
+	}
+	
+	public static LinkedListNode createLinkedList() {
 		/* Create linked list */
-		int[] vals = {33,9,2,3,10,10389,838,874578,5};
+		int[] vals = {3, 5, 8, 5, 10, 2, 1};
 		LinkedListNode head = new LinkedListNode(vals[0], null, null);
 		LinkedListNode current = head;
 		for (int i = 1; i < vals.length; i++) {
 			current = new LinkedListNode(vals[i], null, current);
 		}
-		System.out.println(head.printForward());
-		
-		/* Partition */
-		LinkedListNode h = partition(head, 3);
-		
-		/* Print Result */
-		System.out.println(h.printForward());
+		return head;
 	}
 	
 	/* Pass in the head of the linked list and the value to partition around */
-	public static LinkedListNode partition(LinkedListNode node, int x) {
+	public static LinkedListNode partition1(LinkedListNode node, int x) {
 		LinkedListNode beforeStart = null;
 		LinkedListNode beforeEnd = null;
 		LinkedListNode afterStart = null;
@@ -74,5 +82,60 @@ public class Partition {
 		
 		beforeEnd.next = afterStart;
 		return beforeStart;
+	}
+	
+	public static LinkedListNode partition2(LinkedListNode node, int x) {
+		LinkedListNode beforeStart = null;
+		LinkedListNode afterStart = null;
+		
+		/* Partition list */
+		while (node != null) {
+			LinkedListNode next = node.next;
+			if (node.data < x) {
+				/* Insert node into start of before list */
+				node.next = beforeStart;
+				beforeStart = node;	
+			} else {
+				/* Insert node into front of after list */
+				node.next = afterStart;
+				afterStart = node;
+			}	
+			node = next;
+		}
+		
+		/* Merge before list and after list */
+		if (beforeStart == null) {
+			return afterStart;
+		}
+		
+		LinkedListNode head = beforeStart;
+		while (beforeStart.next != null) {
+			beforeStart = beforeStart.next;
+		}
+		beforeStart.next = afterStart;
+		return head;
+	}
+	
+	public static LinkedListNode partition3(LinkedListNode node, int x) {
+		LinkedListNode head = node;
+		LinkedListNode tail = node;
+		
+		/* Partition list */
+		while (node != null) {
+			LinkedListNode next = node.next;
+			if (node.data < x) {
+				/* Insert node at head. */
+				node.next = head;
+				head = node;
+			} else {
+				/* Insert node at tail. */
+				tail.next = node;
+				tail = node;
+			}	
+			node = next;
+		}
+		tail.next = null;
+		
+		return head;
 	}
 }
