84a0fecf6479e5a187064885fa343f6faf9458da
Sunil Soni <sunilsoni@live.in>
2017-10-29 09:32:07 +0530

    SumLists

--- src/com/ctci6/ch02/SumLists.java
+++ src/com/ctci6/ch02/SumLists.java
@@ -56,7 +56,9 @@ public class SumLists {
 		System.out.println("lA3  :  " + lA3.printForward());
 		System.out.println("lB3  :  " + lB3.printForward());
 		System.out.println("=============================");
-		LinkedListNode list3 = addLists(lA1, lB1, 0);
+		
+		//LinkedListNode list3 = addLists(lA1, lB1, 0);
+		LinkedListNode list3 = addLists(lA1, lB1);
 
 		System.out.println("lA1  :  " + lA1.printForward());
 		System.out.println("lB1  :+ " + lB1.printForward());
@@ -136,5 +138,74 @@ public class SumLists {
 		}
 		return value + node.data;
 	}
+	
+	//
+	private static int length(LinkedListNode l) {
+		if (l == null) {
+			return 0;
+		} else {
+			return 1 + length(l.next);
+		}
+	}
+	
+	private static PartialSum addListsHelper1(LinkedListNode l1, LinkedListNode l2) {
+		if (l1 == null && l2 == null) {
+			PartialSum sum = new PartialSum();
+			return sum;
+		}
+		PartialSum sum = addListsHelper1(l1.next, l2.next);
+		int val = sum.carry + l1.data + l2.data;
+		LinkedListNode full_result = insertBefore(sum.sum, val % 10);
+		sum.sum = full_result;
+		sum.carry = val / 10;
+		return sum;
+	}
+	
+	private static LinkedListNode addLists(LinkedListNode l1, LinkedListNode l2) {
+		int len1 = length(l1);
+		int len2 = length(l2);
+		if (len1 < len2) {
+			l1 = padList(l1, len2 - len1);
+		} else {
+			l2 = padList(l2, len1 - len2);
+		}
+		PartialSum sum = addListsHelper1(l1, l2);
+		if (sum.carry == 0) {
+			return sum.sum;
+		} else {
+			LinkedListNode result = insertBefore(sum.sum, sum.carry);
+			return result;
+		}
+	}	
+	
+	private static LinkedListNode padList(LinkedListNode l, int padding) {
+		LinkedListNode head = l;
+		for (int i = 0; i < padding; i++) {
+			head = insertBefore(head, 0);
+		}
+		return head;
+	}
+	
+	private static LinkedListNode insertBefore(LinkedListNode list, int data) {
+		LinkedListNode node = new LinkedListNode(data);
+		if (list != null) {
+			node.next = list;
+		}
+		return node;
+	}
+	
+	public static int linkedListToInt1(LinkedListNode node) {
+		int value = 0;
+		while (node != null) {
+			value = value * 10 + node.data;
+			node = node.next;
+		}
+		return value;
+	}	
 
+}
+
+class PartialSum {
+	public LinkedListNode sum = null;
+	public int carry = 0;
 }
