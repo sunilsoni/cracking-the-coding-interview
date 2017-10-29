b6a098f45fcebd047dc4ec8df5a19564362e8454
Sunil Soni <sunilsoni@live.in>
2017-10-29 09:36:29 +0530

    SumLists

--- src/com/ctci6/ch02/SumLists.java
+++ src/com/ctci6/ch02/SumLists.java
@@ -139,7 +139,7 @@ public class SumLists {
 		return value + node.data;
 	}
 	
-	//
+	//Follow Up
 	private static int length(LinkedListNode l) {
 		if (l == null) {
 			return 0;
@@ -153,23 +153,38 @@ public class SumLists {
 			PartialSum sum = new PartialSum();
 			return sum;
 		}
+		
+		/* Add smaller digits recursively */
 		PartialSum sum = addListsHelper1(l1.next, l2.next);
+		
+		/* Add carry to current data */
 		int val = sum.carry + l1.data + l2.data;
+		
+		/* Insert sum of current digits */
 		LinkedListNode full_result = insertBefore(sum.sum, val % 10);
+		
+		/* Return sum so far, and the carry value */
 		sum.sum = full_result;
 		sum.carry = val / 10;
 		return sum;
 	}
 	
+
 	private static LinkedListNode addLists(LinkedListNode l1, LinkedListNode l2) {
 		int len1 = length(l1);
 		int len2 = length(l2);
+		/* Pad the shorter list with zeros - see note (1) */
 		if (len1 < len2) {
 			l1 = padList(l1, len2 - len1);
 		} else {
 			l2 = padList(l2, len1 - len2);
 		}
+		
+		/* Add lists */
 		PartialSum sum = addListsHelper1(l1, l2);
+		
+		/* I f there was a carry value left over, insert this at the front of the list.
+		* Otherwise, just return the linked list. */
 		if (sum.carry == 0) {
 			return sum.sum;
 		} else {
@@ -178,6 +193,7 @@ public class SumLists {
 		}
 	}	
 	
+	/* Pad the list with zeros */
 	private static LinkedListNode padList(LinkedListNode l, int padding) {
 		LinkedListNode head = l;
 		for (int i = 0; i < padding; i++) {
@@ -186,6 +202,7 @@ public class SumLists {
 		return head;
 	}
 	
+	/* Helper function to insert node in the front of a linked list */
 	private static LinkedListNode insertBefore(LinkedListNode list, int data) {
 		LinkedListNode node = new LinkedListNode(data);
 		if (list != null) {
