4a50c947f5e4ab52dadfaa28f5535b5151476cc1
Sunil Soni <sunilsoni@live.in>
2017-11-12 09:52:23 +0530

    Palindrome

--- src/com/ctci6/ch02/Palindrome.java
+++ src/com/ctci6/ch02/Palindrome.java
@@ -27,15 +27,19 @@ public class Palindrome {
 				nodes[i].setPrevious(nodes[i - 1]);
 			}
 		}
-		// nodes[length - 2].data = 9; // Uncomment to ruin palindrome
+		
+		//nodes[length - 2].data = 9; // Uncomment to ruin palindrome
 		
 		LinkedListNode head = nodes[0];
 		System.out.println(head.printForward());
 		//System.out.println(isPalindrome1(head));
-		System.out.println(isPalindrome2(head));
+		//System.out.println(isPalindrome2(head));
+		System.out.println(isPalindrome3(head));
 	}
 	
-	//Solution #1: Reverse and Compare
+	/**Solution #1: Reverse and Compare:Our first solution is to reverse the linked list and compare the reversed list to the original list. 
+	 * If they're the same, the lists are identical.	
+	*/
 	public static boolean isPalindrome1(LinkedListNode head) {
 		LinkedListNode reversed = reverseAndClone(head);
 		System.out.println("reversed-->"+reversed.printForward());
@@ -51,7 +55,7 @@ public class Palindrome {
 			node = node.next;
 		}
 		return head;
-	}	
+	}
 		
 	public static boolean isEqual(LinkedListNode one, LinkedListNode two) {
 		while (one != null && two != null) {
@@ -74,8 +78,7 @@ public class Palindrome {
 		/* Push elements from first half of linked list onto stack. When fast runner
 		 * (which is moving at 2x speed) reaches the end of the linked list, then we
 		 * know we're at the middle*/
-		while (fast != null && fast.next != null) {
-			
+		while (fast != null && fast.next != null) {			
 			stack.push(slow.data);
 			slow = slow.next;
 			fast = fast.next.next;	
@@ -91,10 +94,61 @@ public class Palindrome {
 		while (slow != null) {
 			int top = stack.pop().intValue();
 			if (top != slow.data) {
-				return false;
+				return false; 
 			}
 			slow = slow.next;
 		}
 		return true;
+	}
+	
+	
+	//Solution #3: Recursive Approach
+	public static class Result {
+		public LinkedListNode node;
+		public boolean result;
+		public Result(LinkedListNode n, boolean res) {
+			node = n;
+			result = res;
+		}
+	}
+
+	public static Result isPalindromeRecurse(LinkedListNode head, int length) {
+		if (head == null || length <= 0) { // Even number of nodes
+			return new Result(head, true);
+		} else if (length == 1) { // Odd number of nodes
+			return new Result(head.next, true);
+		}
+		
+		/* Recurse on sublist. */
+		Result res = isPalindromeRecurse(head.next, length - 2);
+		
+		/* If child calls are not a palindrome, pass back up 
+		 * a failure. */
+		if (!res.result || res.node == null) {
+			return res;
+		}
+		
+		/* Check if matches corresponding node on other side. */
+		res.result = (head.data == res.node.data); 
+		
+		/* Return corresponding node. */
+		res.node = res.node.next;
+		
+		return res;
+	}
+	
+	public static int lengthOfList(LinkedListNode n) {
+		int size = 0;
+		while (n != null) {
+			size++;
+			n = n.next;
+		}
+		return size;
+	}
+	
+	public static boolean isPalindrome3(LinkedListNode head) {
+		int length = lengthOfList(head);
+		Result p = isPalindromeRecurse(head, length);
+		return p.result;
 	}
 }
