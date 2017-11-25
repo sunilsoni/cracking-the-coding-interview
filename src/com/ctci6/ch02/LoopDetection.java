84a4d46180e62920b3a31b7db1a78a08a0f081f1
Sunil Soni <sunilsoni@live.in>
2017-11-25 08:14:57 +0530

    LoopDetection

--- src/com/ctci6/ch02/LoopDetection.java
+++ src/com/ctci6/ch02/LoopDetection.java
@@ -1,5 +1,7 @@
 package com.ctci6.ch02;
 
+import com.ctci6.utils.LinkedListNode;
+
 /**
  * Given a circular linked list, implement an algorithm that returns the node at the beginning of the loop.
  * <p>DEFINITION:</p>
@@ -14,8 +16,56 @@ package com.ctci6.ch02;
 public class LoopDetection {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int list_length = 100;
+		int k = 10;
+		
+		// Create linked list
+		LinkedListNode[] nodes = new LinkedListNode[list_length];
+		for (int i = 0; i < list_length; i++) {
+			nodes[i] = new LinkedListNode(i, null, i > 0 ? nodes[i - 1] : null);
+		}
+		
+		// Create loop;
+		nodes[list_length - 1].next = nodes[list_length - k];
+		
+		LinkedListNode loop = FindBeginning(nodes[0]);
+		if (loop == null) {
+			System.out.println("No Cycle.");
+		} else {
+			System.out.println(loop.data);
+		}
+	}
+	
+	
+	public static LinkedListNode FindBeginning(LinkedListNode head) {
+		LinkedListNode slow = head;
+		LinkedListNode fast = head; 
+		
+		// Find meeting point
+		while (fast != null && fast.next != null) { 
+			slow = slow.next; 
+			fast = fast.next.next;
+			if (slow == fast) {
+				break;
+			}
+		}
 
+		// Error check - there is no meeting point, and therefore no loop
+		if (fast == null || fast.next == null) {
+			return null;
+		}
+
+		/* Move slow to Head. Keep fast at Meeting Point. Each are k steps
+		/* from the Loop Start. If they move at the same pace, they must
+		 * meet at Loop Start. */
+		slow = head; 
+		while (slow != fast) { 
+			slow = slow.next; 
+			fast = fast.next; 
+		}
+		
+		// Both now point to the start of the loop.
+		return fast;
 	}
 
 }
