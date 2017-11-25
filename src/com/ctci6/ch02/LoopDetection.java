3be4d350478a4aa559257bdae2eefed056a2bfa4
Sunil Soni <sunilsoni@live.in>
2017-11-25 08:12:19 +0530

    LoopDetection

--- src/com/ctci6/ch02/LoopDetection.java
+++ src/com/ctci6/ch02/LoopDetection.java
@@ -1,5 +1,16 @@
 package com.ctci6.ch02;
 
+/**
+ * Given a circular linked list, implement an algorithm that returns the node at the beginning of the loop.
+ * <p>DEFINITION:</p>
+ * <blockquote><pre>
+ * Circular linked list: A (corrupt) linked list in which a node's next pointer points to an earlier node, so as to make a loop in the linked list.
+ * Input: A - > B - > C - > D - > E - > C [the same C as earlier]
+ * Output:C
+ * </blockquote></pre>
+
+ * @author Sunil
+ */
 public class LoopDetection {
 
 	public static void main(String[] args) {
