200ba22bdf3f97883470b0620841dd5d5bcfb80b
Sunil Soni <sunilsoni@live.in>
2018-01-06 11:01:21 +0530

    SortStack

--- src/com/ctci6/ch03/SortStack.java
+++ src/com/ctci6/ch03/SortStack.java
@@ -1,12 +1,90 @@
 package com.ctci6.ch03;
 
+import com.ctci6.utils.AssortedMethods;
+import java.util.Stack;
 
 /**
  * Write a program to sort a stack such that the smallest items are on the top. You can use an additional temporary stack,
  * but you may not copy the elements into any other data structure (such as an array).
  * The stack supports the following operations: push, pop, peek, and isEmpty.
+ * Time Complexity: O ( N^2)
+ * Space Complexity: O ( N).
  *
  * @author Sunil
  */
 public class SortStack {
+
+    public static void main(String [] args) {
+        Stack<Integer> s = new Stack<Integer>();
+        for (int i = 0; i < 10; i++) {
+            int r = AssortedMethods.randomIntInRange(0,  1000);
+            s.push(r);
+        }
+
+        sort(s);
+
+        while(!s.isEmpty()) {
+            System.out.println(s.pop());
+        }
+    }
+
+    public static Stack<Integer> mergesort(Stack<Integer> inStack) {
+        if (inStack.size() <= 1) {
+            return inStack;
+        }
+
+        Stack<Integer> left = new Stack<Integer>();
+        Stack<Integer> right = new Stack<Integer>();
+        int count = 0;
+        while (inStack.size() != 0) {
+            count++;
+            if (count % 2 == 0) {
+                left.push(inStack.pop());
+            } else {
+                right.push(inStack.pop());
+            }
+        }
+
+        left = mergesort(left);
+        right = mergesort(right);
+
+        while (left.size() > 0 || right.size() > 0) {
+            if (left.size() == 0) {
+                inStack.push(right.pop());
+            } else if (right.size() == 0) {
+                inStack.push(left.pop());
+            } else if (right.peek().compareTo(left.peek()) <= 0) {
+                inStack.push(left.pop());
+            } else {
+                inStack.push(right.pop());
+            }
+        }
+
+        Stack<Integer> reverseStack = new Stack<Integer>();
+        while (inStack.size() > 0) {
+            reverseStack.push(inStack.pop());
+        }
+
+        return reverseStack;
+    }
+
+    public static void sort(Stack<Integer> s) {
+        Stack<Integer> r = new Stack<Integer>();
+        while(!s.isEmpty()) {
+            /* Insert each element in s in sorted order into r. */
+            int tmp = s.pop();
+            while(!r.isEmpty() && r.peek() > tmp) {
+                s.push(r.pop());
+            }
+            r.push(tmp);
+        }
+
+        /* Copy the elements back. */
+        while (!r.isEmpty()) {
+            s.push(r.pop());
+        }
+    }
+
+
+
 }
